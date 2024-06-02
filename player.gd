a8c482a74acc6e27932115dced59f4595417e84d
Ben Miles <bcgm3studios@gmail.com>
2024-06-02 11:46:30 -0400

    Testing apply_impulse()

--- player.gd
+++ player.gd
@@ -1,11 +1,5 @@
 extends RigidBody2D
 
-
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+	apply_impulse(Vector2(25,5))
