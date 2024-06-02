6428f14dd98c6a23c8a9b392318741e3705a7e42
Ben Miles <bcgm3studios@gmail.com>
2024-06-02 12:00:56 -0400

    _process() VS _physics_process()

--- player.gd
+++ player.gd
@@ -3,3 +3,11 @@ extends RigidBody2D
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	apply_impulse(Vector2(25,5))
+
+# Runs every frame (effected by framerate)
+func _process(delta):
+	pass
+
+# Runs every time there is a phsyics calculation -- not effected by framerate
+func _physics_process(delta):
+	apply_force(Vector2(25,5))
